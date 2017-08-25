9fc1bd93381e70eaa06b972aebf8cbf8711ce34f
kitti piyapan <kitti.piyapan@gmail.com>
2017-08-25 14:54:30 +0700

    add scope email

--- components/MenuProfile.js
+++ components/MenuProfile.js
@@ -86,7 +86,7 @@ export default class MenuProfile extends Component {
               appId={facebookAppId}
               autoLoad={false}
               fields="name,email,picture,first_name,last_name"
-              scope="public_profile"
+              scope="public_profile, email"
               callback={this.responseFacebook}
               textButton="Login"
               cssClass="loginBtn loginBtn--facebook"
