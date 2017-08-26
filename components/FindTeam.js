9f053f9286a1ef726076b47d60c055d4a35a0d45
kitti piyapan <kitti.piyapan@gmail.com>
2017-08-26 15:43:41 +0700

    card

--- components/FindTeam.js
+++ components/FindTeam.js
@@ -5,9 +5,26 @@ export default class FindTeam extends Component {
   render() {
     return (
       <Card>
-        <div>555</div>
+        <div className="poster-header">
+          <a className="poster-avatar">
+            <img className="avatar" src="https://thedotascene.files.wordpress.com/2015/08/rtz.jpg?w=240" />
+          </a>
+          <a className="poster-name">5555</a>
+        </div>
         <style jsx>{`
-          .gg
+          .avatar-avatar {
+            float: left;
+            margin-right: 6px;
+            border-radius: 2px;
+          }
+          .poster-avatar > .avatar {
+            width: 40px;
+            height: 40px;
+          }
+          .poster-name {
+            float: left;
+            margin-left: 12px;
+          }
         `}</style>
       </Card>
     )
